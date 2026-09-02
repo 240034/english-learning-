d2a2368724fad8e273df7fb802394c775ca428bb
240034 <240034@astanait.edu.kz>
2026-09-02 15:10:52 +0500

    Create words-data.js with daily vocabulary
    Added daily words with translations, images, and audio for September.

---
+++ js/words-data.js
@@ -0,0 +1,15 @@
+// js/words-data.js
+export const dailyWords = {
+    "September 1": [
+        { word: "hardworking", translation: "трудолюбивый", image: "images/hardworking.jpg", audio: "audio/hardworking.mp3" },
+        { word: "sofa", translation: "диван", image: "images/sofa.jpg", audio: "audio/sofa.mp3" },
+        { word: "umbrella", translation: "зонт", image: "images/umbrella.jpg", audio: "audio/umbrella.mp3" }
+    ],
+    "September 2": [
+        { word: "patience", translation: "терпение", image: "images/patience.jpg", audio: "audio/patience.mp3" },
+        { word: "adventure", translation: "приключение", image: "images/adventure.jpg", audio: "audio/adventure.mp3" }
+    ],
+    "September 3": [
+        { word: "curious", translation: "любознательный", image: "images/curious.jpg", audio: "audio/curious.mp3" }
+    ]
+};
